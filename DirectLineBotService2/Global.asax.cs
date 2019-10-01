b63b58285cecb9df642417bc8a7369ae534c7ac9
Sunny Nayak <sunaya@microsoft.com>
2019-10-01 13:41:45 -0700

    clean-up

--- DirectLineBotService2/Global.asax.cs
+++ DirectLineBotService2/Global.asax.cs
@@ -15,7 +15,6 @@ namespace DirectLineBotService2
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
